4c3dd028062ad319b9c255d5c21eddc35a6f1809
SoheeP <psh950502@naver.com>
2024-10-23 22:36:58 +0900

    Add: react 버전 수정, react-query, prettier 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.wikia.nocookie.net",
+        pathname: "/catchteenieping/**"
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/u/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
