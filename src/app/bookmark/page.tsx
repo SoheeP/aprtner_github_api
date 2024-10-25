e862df7c95294d65c4f0d278a3bce213e5527499
SoheeP <psh950502@naver.com>
2024-10-25 22:27:06 +0900

    Fix: localstorage not defined(SSR) 문제 수정

--- src/app/bookmark/page.tsx
+++ src/app/bookmark/page.tsx
@@ -3,22 +3,29 @@ import UserCard from "../user/userCard";
 import { User } from "../user/type";
 import { useDisplayUserStore } from "@/providers/displayUserStoreProvider";
 import { bookmarkToggle, getBookmarkNodeIds } from "../user/service";
+import { useEffect, useState } from "react";
 
 const Bookmark = () => {
   const { actions } = useDisplayUserStore((state) => state);
-  const bookmarkNodeIds = getBookmarkNodeIds();
+  const [bookmarkUser, setBookmarkUser] = useState<User[]>([])
+
+  useEffect(() => {
+    setBookmarkUser(getBookmarkNodeIds());
+  }, [])
+
   const onBookmarkClick = (user:User) => {
     bookmarkToggle(user);
     actions.updateDisplayUser({
       ...user,
       isLiked: !user.isLiked
     })
+    setBookmarkUser(getBookmarkNodeIds());
   }
   return (
     <div className="p-4">
       <div className="grid grid-cols-2 gap-4">
         {
-          bookmarkNodeIds.map((user: User, index: number) => <UserCard key={`${user.id}-${user.name}-${index}`} user={user} onBookmarkClick={onBookmarkClick} />)
+          bookmarkUser?.map((user: User, index: number) => <UserCard key={`${user.id}-${user.name}-${index}`} user={user} onBookmarkClick={onBookmarkClick} />)
         }
       </div>
     </div>
